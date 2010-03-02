89e8cedea09d9f9c21ced8db4b88523385a0407b
M. Auayan <sxtxixtxcxh@gmail.com>
2010-03-02 00:21:32 -0800

    converted tabs *cough* to spaces...

--- framework/framework.php
+++ framework/framework.php
@@ -3,23 +3,23 @@
 class Framework {
   
   public static 
-	  $views_extension = 'php',
+    $views_extension = 'php',
     $current_controller_object = null,
-	  $controller = null,
-	  $cache = null,
-	  $prefs = '',
-	  $tmp_path = '';
+    $controller = null,
+    $cache = null,
+    $prefs = '',
+    $tmp_path = '';
     
   function boot(){
     if ( !defined('APP_ROOT') ) {
-    	define('APP_ROOT', dirname(dirname(__FILE__)));
+      define('APP_ROOT', dirname(dirname(__FILE__)));
     }
     self::$tmp_path = APP_ROOT.'/tmp';
 
     // initialize preference system
     include APP_ROOT.'/framework/lib/preferences.php';
-		self::$prefs = new PreferenceCollection( array(self::$tmp_path) );
-		self::$prefs->read('cache', true, self::$tmp_path);
+    self::$prefs = new PreferenceCollection( array(self::$tmp_path) );
+    self::$prefs->read('cache', true, self::$tmp_path);
 
     Timer::start();
   }
@@ -31,12 +31,12 @@ class Controller {
   private $content_for_layout = null;
   public $view_file;
   public $layout = '';
-	
-	function render_partial($file, $locals = array()){
+  
+  function render_partial($file, $locals = array()){
     $file = APP_ROOT.'/_partials/'.$file.'.'.Framework::$views_extension;
     $this->render_file($file, $locals);
-	}
-		
+  }
+    
   function render_file($file, $locals = array()){
     
     // make sure the file exists
@@ -45,39 +45,39 @@ class Controller {
       
       // store each of the object's properties as vars accessible 
       // to the file withouth a "$this->" in front of it.
-			if ( is_object($this) ) {
-				foreach( $this as $tmp_key => $tmp_value ) {
-					${$tmp_key} = &$this->$tmp_key;
-				}        
-				unset($tmp_key, $tmp_value);
-			}
+      if ( is_object($this) ) {
+        foreach( $this as $tmp_key => $tmp_value ) {
+          ${$tmp_key} = &$this->$tmp_key;
+        }        
+        unset($tmp_key, $tmp_value);
+      }
 
       // store the content for layout if it's set...
       
-			if ( $this->content_for_layout !== null ) {
-				$content_for_layout = &$this->content_for_layout;
-			}
+      if ( $this->content_for_layout !== null ) {
+        $content_for_layout = &$this->content_for_layout;
+      }
 
       // store var in the locals array so it will be 
       // accessible in the file..
-			if ( count($locals) ) {
-				foreach( $locals as $tmp_key => $tmp_value ) {
-					${$tmp_key} = &$locals[$tmp_key];
-				}
-				unset($tmp_key, $tmp_value);
-			}
-			
-			unset($locals);
-			
-			include( $file );
+      if ( count($locals) ) {
+        foreach( $locals as $tmp_key => $tmp_value ) {
+          ${$tmp_key} = &$locals[$tmp_key];
+        }
+        unset($tmp_key, $tmp_value);
+      }
+      
+      unset($locals);
+      
+      include( $file );
 
       return true;
       
-		}else{
-		  
-		  // if the file doesn't exist... 
-			return false;
-			
+    }else{
+      
+      // if the file doesn't exist... 
+      return false;
+      
     }
     
   }
@@ -96,7 +96,7 @@ class Controller {
     $this->content_for_layout = ob_get_contents();
     
     // and clean up.
-		ob_end_clean();
+    ob_end_clean();
     
     if ( Timer::$started ) ob_start();
     
@@ -107,12 +107,12 @@ class Controller {
     
     // if you opt out a layout or if the layout doesn't render
     // then just spit out the contents of the view
-		if ( $this->layout === false || !$this->render_file($layout_file) ) {
-			echo $this->content_for_layout;
-		}
-		
-		if ( Timer::$started ) ob_end_flush();
-		
+    if ( $this->layout === false || !$this->render_file($layout_file) ) {
+      echo $this->content_for_layout;
+    }
+    
+    if ( Timer::$started ) ob_end_flush();
+    
   }
   
   function parse_request(){
@@ -140,56 +140,56 @@ class Controller {
   }
   
   function raise ($message, $details, $code) {
-		throw new FrameworkError($message, $details, $code);
-	}
+    throw new FrameworkError($message, $details, $code);
+  }
   
   function process_exception( $object ){
     
-		$this->error = $object;
-		$this->message = $object->getMessage();
-		$this->details = $object->getDetails();
-		$this->code = $object->getCode();
-		$this->trace = $object->getTrace();
+    $this->error = $object;
+    $this->message = $object->getMessage();
+    $this->details = $object->getDetails();
+    $this->code = $object->getCode();
+    $this->trace = $object->getTrace();
 
-		if ( $this->code != 0 ) {
-			header(StatusCode::http_header_for($this->code));
-		}
-		
-		$paths = array(
-			'_errors',
-			'framework/_errors',
-		);
-		
-		foreach( $paths as $path ) {
+    if ( $this->code != 0 ) {
+      header(StatusCode::http_header_for($this->code));
+    }
+    
+    $paths = array(
+      '_errors',
+      'framework/_errors',
+    );
+    
+    foreach( $paths as $path ) {
       if ( is_file(APP_ROOT.'/'.$path.'/'.$this->code.'.'.Framework::$views_extension) ) {
-  			$view_file = APP_ROOT.'/'.$path.'/'.$this->code.'.'.Framework::$views_extension;
-  			break;
-  		} elseif ( is_file(APP_ROOT.'/'.$path.'/default.'.Framework::$views_extension) ) {
-				$view_file = APP_ROOT.'/'.$path.'/default.'.Framework::$views_extension;
-				break;
-			}
-		}
-		
-		if(StatusCode::can_has_body($this->code)){
-  		$this->render_file($view_file);
-		}
+        $view_file = APP_ROOT.'/'.$path.'/'.$this->code.'.'.Framework::$views_extension;
+        break;
+      } elseif ( is_file(APP_ROOT.'/'.$path.'/default.'.Framework::$views_extension) ) {
+        $view_file = APP_ROOT.'/'.$path.'/default.'.Framework::$views_extension;
+        break;
+      }
+    }
+    
+    if(StatusCode::can_has_body($this->code)){
+      $this->render_file($view_file);
+    }
     
   }
 
 }
 
 class FrameworkError extends Exception {
-	
-	protected $details = null;
-	
+  
+  protected $details = null;
+  
   function __construct($message = null, $details = null, $code = 0) {
-		$this->details = $details;
-		parent::__construct($message, $code);
-	}
-	
-	public function getDetails () {
-		return $this->details;
-	}
+    $this->details = $details;
+    parent::__construct($message, $code);
+  }
+  
+  public function getDetails () {
+    return $this->details;
+  }
 
 }
 
@@ -200,28 +200,28 @@ class FrameworkError extends Exception {
 
 function render_partial(){
   $args = func_get_args();
-	return call_user_func_array(array(Framework::$current_controller_object, 'render_partial'), $args);
+  return call_user_func_array(array(Framework::$current_controller_object, 'render_partial'), $args);
 }
 
 function raise(){
   $args = func_get_args();
-	return call_user_func_array(array(Framework::$current_controller_object, 'raise'), $args);
+  return call_user_func_array(array(Framework::$current_controller_object, 'raise'), $args);
 }
 
 function __autoload ($class_name) {
 
   // check cache if specified class has been found before
-	if ( !empty(Framework::$cache->autoload[$class_name]) ) {
-		if ( is_file(Framework::$cache->autoload[$class_name]) ) {
-			include_once(Framework::$cache->autoload[$class_name]);
-			$found = true;
-		} else {
-			unset(Framework::$cache->autoload[$class_name]);
-			$save_cache = true;
-		}
-	}
-	
-	
+  if ( !empty(Framework::$cache->autoload[$class_name]) ) {
+    if ( is_file(Framework::$cache->autoload[$class_name]) ) {
+      include_once(Framework::$cache->autoload[$class_name]);
+      $found = true;
+    } else {
+      unset(Framework::$cache->autoload[$class_name]);
+      $save_cache = true;
+    }
+  }
+  
+  
   
   $autoload_paths  = array( APP_ROOT.'/lib/', 
                             APP_ROOT.'/framework/lib/',
@@ -250,9 +250,9 @@ function __autoload ($class_name) {
   }
   
   if ( !empty($save_cache) ) {
-		Framework::$prefs->cache->save();
-	}
-	
+    Framework::$prefs->cache->save();
+  }
+  
   
 }
 
--- framework/lib/timer.lib.php
+++ framework/lib/timer.lib.php
@@ -33,117 +33,117 @@
 
 class Timer {
 
-	public static 	
-		
-		# number of decimals to calculate
-		$digits = 3,
-		
-		# number of decimals to calulate requests per second to
-		$req_digits = 2,
-	
-		# output pattern used by end()
-		#  %s = seconds since start
-		#  %r = requests per second
-		$pattern = '%s sec — %r reqs/sec',
-	
-	
-		# start time
-		$start = null,
-		
-		# end time
-		$end = null,
-	
-		# execution time
-		$time = null,
-		
-		# requests per second at current execution time
-		$requests = null,
-	
-		# saved output from end()
-		$output = null,
-		
-		# timer is started? true or false
-		$started = false;
-	
-	
-	/**
-	 * Start
-	 * @param   digits   number of decimals to calculate
-	 * @return  nothing
-	 */
-	function start ($digits = null) {
-		if ( !empty($digits) ) self::$digits = $digits;
-		self::$start = microtime(true);
-		self::$started = true;
-	}
-	
-	/**
-	 * Calculate time since start and return using defined pattern
-	 * @param   digits    number of decimals to calculate
-	 * @param   pattern   output pattern ("%s" is replaced by time)
-	 * @return  time with microseconds since start, formatted using pattern
-	 */
-	function end ($digits = null, $pattern = null) {
-		self::$end = microtime(true);
-		if ( !preg_match("/[0-9]{1,3}/", $digits) ) $digits = self::$digits;
-		if ( strpos($pattern, '%s') === false ) $pattern = self::$pattern;
-		self::$time = number_format( (self::$end - self::$start), $digits);
-		self::$output = str_replace('%s', self::$time, $pattern);
-		if ( strpos($pattern, '%r') !== false ) {
-			self::$requests = number_format( (1 / self::$time), self::$req_digits);
-			self::$output = str_replace('%r', self::$requests, self::$output);
-		}
-		return self::$output;
-	}
-	
-	/**
-	 * Calculate time since start and return plain time
-	 * @param   digits    number of decimals to calculate
-	 * @return  time with microseconds since start
-	 */
-	function term ($digits = null) {
-		self::$end = microtime(true);
-		if ( !preg_match("/[0-9]{1,3}/", $digits) ) $digits = self::$digits;
-		self::$time = number_format( (self::$end - self::$start), $digits);
-		self::$requests = number_format( (1 / self::$time), self::$req_digits);
-		return self::$time;
-	}
-	
-	/**
-	 * Output detailed timing info for debugging
-	 * @return  detailed execution timing info
-	 */
-	function debug () {
-		$lf = "\n";
-		
-		$start  = 'Start time: ';
-		$start .= (self::$started) ? self::$start.' ('.self::prettify_time(self::$start).')' : 'Not started.' ;
-		
-		$end  = 'End time:   ';
-		$end .= (self::$end !== null) ? self::$end.' ('.self::prettify_time(self::$end).')' : 'Not ended.' ;
-		
-		$time = 'Script execution: ';
-		$reqs = 'Requests/second: ';
-		
-		if ( self::$started && self::$end !== null ) {
-			$time .= $t = number_format( (self::$end - self::$start), 5);
-			$reqs .= number_format( (1 / $t), self::$req_digits);
-		} else {
-			$time .= 'Unknown';
-			$reqs .= 'Unknown';
-		}
-		
-		return $time.$lf.$reqs.$lf.$start.$lf.$end;
-	}
-	
-	function prettify_time ($input, $format = 'd-M-Y H:i:s') {
-		if ( strpos($input, '.') !== false ) {
-			$time = explode('.', $input);
-			return date($format, $time[0]).'.'.$time[1];
-		} else {
-			return date($format, $time[0]);
-		}
-	}
+  public static   
+    
+    # number of decimals to calculate
+    $digits = 3,
+    
+    # number of decimals to calulate requests per second to
+    $req_digits = 2,
+  
+    # output pattern used by end()
+    #  %s = seconds since start
+    #  %r = requests per second
+    $pattern = '%s sec — %r reqs/sec',
+  
+  
+    # start time
+    $start = null,
+    
+    # end time
+    $end = null,
+  
+    # execution time
+    $time = null,
+    
+    # requests per second at current execution time
+    $requests = null,
+  
+    # saved output from end()
+    $output = null,
+    
+    # timer is started? true or false
+    $started = false;
+  
+  
+  /**
+   * Start
+   * @param   digits   number of decimals to calculate
+   * @return  nothing
+   */
+  function start ($digits = null) {
+    if ( !empty($digits) ) self::$digits = $digits;
+    self::$start = microtime(true);
+    self::$started = true;
+  }
+  
+  /**
+   * Calculate time since start and return using defined pattern
+   * @param   digits    number of decimals to calculate
+   * @param   pattern   output pattern ("%s" is replaced by time)
+   * @return  time with microseconds since start, formatted using pattern
+   */
+  function end ($digits = null, $pattern = null) {
+    self::$end = microtime(true);
+    if ( !preg_match("/[0-9]{1,3}/", $digits) ) $digits = self::$digits;
+    if ( strpos($pattern, '%s') === false ) $pattern = self::$pattern;
+    self::$time = number_format( (self::$end - self::$start), $digits);
+    self::$output = str_replace('%s', self::$time, $pattern);
+    if ( strpos($pattern, '%r') !== false ) {
+      self::$requests = number_format( (1 / self::$time), self::$req_digits);
+      self::$output = str_replace('%r', self::$requests, self::$output);
+    }
+    return self::$output;
+  }
+  
+  /**
+   * Calculate time since start and return plain time
+   * @param   digits    number of decimals to calculate
+   * @return  time with microseconds since start
+   */
+  function term ($digits = null) {
+    self::$end = microtime(true);
+    if ( !preg_match("/[0-9]{1,3}/", $digits) ) $digits = self::$digits;
+    self::$time = number_format( (self::$end - self::$start), $digits);
+    self::$requests = number_format( (1 / self::$time), self::$req_digits);
+    return self::$time;
+  }
+  
+  /**
+   * Output detailed timing info for debugging
+   * @return  detailed execution timing info
+   */
+  function debug () {
+    $lf = "\n";
+    
+    $start  = 'Start time: ';
+    $start .= (self::$started) ? self::$start.' ('.self::prettify_time(self::$start).')' : 'Not started.' ;
+    
+    $end  = 'End time:   ';
+    $end .= (self::$end !== null) ? self::$end.' ('.self::prettify_time(self::$end).')' : 'Not ended.' ;
+    
+    $time = 'Script execution: ';
+    $reqs = 'Requests/second: ';
+    
+    if ( self::$started && self::$end !== null ) {
+      $time .= $t = number_format( (self::$end - self::$start), 5);
+      $reqs .= number_format( (1 / $t), self::$req_digits);
+    } else {
+      $time .= 'Unknown';
+      $reqs .= 'Unknown';
+    }
+    
+    return $time.$lf.$reqs.$lf.$start.$lf.$end;
+  }
+  
+  function prettify_time ($input, $format = 'd-M-Y H:i:s') {
+    if ( strpos($input, '.') !== false ) {
+      $time = explode('.', $input);
+      return date($format, $time[0]).'.'.$time[1];
+    } else {
+      return date($format, $time[0]);
+    }
+  }
 }
 
 ?>
