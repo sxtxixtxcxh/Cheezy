8ed90071333fb6aba9ccf495963290d5d3b1ed74
M. Auayan <sxtxixtxcxh@gmail.com>
2010-06-08 12:49:48 -0700

    ignoring query string in uri

--- framework.php
+++ framework.php
@@ -3,26 +3,15 @@
 class Framework {
   
   public static 
-    $views_extension = 'php',
+	  $views_extension = 'php',
     $current_controller_object = null,
-    $controller = null,
-    $cache = null,
-    $prefs = '',
-    $tmp_path = '';
-    
+	  $controller = null;
+
   function boot(){
     if ( !defined('APP_ROOT') ) {
-      define('FRAMEWORK_ROOT', dirname(__FILE__));
-      define('APP_ROOT', dirname(dirname(FRAMEWORK_ROOT)));
+    	define('APP_ROOT', dirname(dirname(__FILE__)));
     }
-    
-    self::$tmp_path = APP_ROOT.'/tmp';
 
-    // initialize preference system
-    include FRAMEWORK_ROOT.'/lib/preferences.php';
-    self::$prefs = new PreferenceCollection( array(self::$tmp_path) );
-    self::$prefs->read('cache', true, self::$tmp_path);
-    self::$cache = self::$prefs->cache;
     Timer::start();
   }
   
@@ -32,14 +21,15 @@ class Framework {
 class Controller {
   private $content_for_layout = null;
   public $view_file;
-  public $page;
   public $layout = '';
-  
-  function render_partial($file, $locals = array()){
-    $file = APP_ROOT.'/_partials/'.$file.'.'.Framework::$views_extension;
-    $this->render_file($file, $locals);
-  }
-    
+	
+	function render_partial($file, $locals = array()){
+	  if ( is_file( APP_ROOT.'/_partials/'.$file.'.'.Framework::$views_extension ) ){
+	    $file = APP_ROOT.'/_partials/'.$file.'.'.Framework::$views_extension;
+	    $this->render_file($file, $locals);
+	  }
+	}
+		
   function render_file($file, $locals = array()){
     
     // make sure the file exists
@@ -48,39 +38,39 @@ class Controller {
       
       // store each of the object's properties as vars accessible 
       // to the file withouth a "$this->" in front of it.
-      if ( is_object($this) ) {
-        foreach( $this as $tmp_key => $tmp_value ) {
-          ${$tmp_key} = &$this->$tmp_key;
-        }        
-        unset($tmp_key, $tmp_value);
-      }
+			if ( is_object($this) ) {
+				foreach( $this as $tmp_key => $tmp_value ) {
+					${$tmp_key} = &$this->$tmp_key;
+				}        
+				unset($tmp_key, $tmp_value);
+			}
 
       // store the content for layout if it's set...
       
-      if ( $this->content_for_layout !== null ) {
-        $content_for_layout = &$this->content_for_layout;
-      }
+			if ( $this->content_for_layout !== null ) {
+				$content_for_layout = &$this->content_for_layout;
+			}
 
       // store var in the locals array so it will be 
       // accessible in the file..
-      if ( count($locals) ) {
-        foreach( $locals as $tmp_key => $tmp_value ) {
-          ${$tmp_key} = &$locals[$tmp_key];
-        }
-        unset($tmp_key, $tmp_value);
-      }
-      
-      unset($locals);
-      
-      include( $file );
+			if ( count($locals) ) {
+				foreach( $locals as $tmp_key => $tmp_value ) {
+					${$tmp_key} = &$locals[$tmp_key];
+				}
+				unset($tmp_key, $tmp_value);
+			}
+			
+			unset($locals);
+			
+			include( $file );
 
       return true;
       
-    }else{
-      
-      // if the file doesn't exist... 
-      return false;
-      
+		}else{
+		  
+		  // if the file doesn't exist... 
+			return false;
+			
     }
     
   }
@@ -99,7 +89,7 @@ class Controller {
     $this->content_for_layout = ob_get_contents();
     
     // and clean up.
-    ob_end_clean();
+		ob_end_clean();
     
     if ( Timer::$started ) ob_start();
     
@@ -110,22 +100,23 @@ class Controller {
     
     // if you opt out a layout or if the layout doesn't render
     // then just spit out the contents of the view
-    if ( $this->layout === false || !$this->render_file($layout_file) ) {
-      echo $this->content_for_layout;
-    }
-    
-    if ( Timer::$started ) ob_end_flush();
-    
+		if ( $this->layout === false || !$this->render_file($layout_file) ) {
+			echo $this->content_for_layout;
+		}
+		
+		if ( Timer::$started ) ob_end_flush();
+		
   }
   
   function parse_request(){
 
     // should use a router, but this works for now.
     // do a little sanitation
-    $this->page = str_replace('..', '', trim($_SERVER['REQUEST_URI'],'/'));
-    if($this->page == '') $this->page = 'home';
+    $uri = explode('?', $_SERVER['REQUEST_URI']);
+    $view = str_replace('..', '', trim($uri[0],'/'));
     
-    $this->view_file = APP_ROOT.'/_pages/'.$this->page.'.'.Framework::$views_extension;
+    if($view == '') $view = 'home';
+    $this->view_file = APP_ROOT.'/_pages/'.$view.'.'.Framework::$views_extension;
 
   }
   
@@ -144,57 +135,56 @@ class Controller {
   }
   
   function raise ($message, $details, $code) {
-    throw new FrameworkError($message, $details, $code);
-  }
+		throw new FrameworkError($message, $details, $code);
+	}
   
   function process_exception( $object ){
     
-    $this->error = $object;
-    $this->message = $object->getMessage();
-    $this->details = $object->getDetails();
-    $this->code = $object->getCode();
-    $this->trace = $object->getTrace();
+		$this->error = $object;
+		$this->message = $object->getMessage();
+		$this->details = $object->getDetails();
+		$this->code = $object->getCode();
+		$this->trace = $object->getTrace();
 
-    if ( $this->code != 0 ) {
-      header(StatusCode::http_header_for($this->code));
-    }
-    
-    $paths = array(
-      APP_ROOT.'/_errors',
-      FRAMEWORK_ROOT.'/_errors',
-    );
-    
-    foreach( $paths as $path ) {
-      if ( is_file($path.'/'.$this->code.'.'.Framework::$views_extension) ) {
-        $view_file = $path.'/'.$this->code.'.'.Framework::$views_extension;
-        break;
-      } elseif ( is_file($path.'/default.'.Framework::$views_extension) ) {
-        $view_file = $path.'/default.'.Framework::$views_extension;
-        break;
-      }
-    }
-    
-    if(StatusCode::can_has_body($this->code)){
-      $this->render_file($view_file);
-    }
+		if ( $this->code != 0 ) {
+			header(StatusCode::http_header_for($this->code));
+		}
+		
+		$paths = array(
+			'_errors',
+			'framework/_errors',
+		);
+		
+		foreach( $paths as $path ) {
+      if ( is_file(APP_ROOT.'/'.$path.'/'.$this->code.'.'.Framework::$views_extension) ) {
+  			$view_file = APP_ROOT.'/'.$path.'/'.$this->code.'.'.Framework::$views_extension;
+  			break;
+  		} elseif ( is_file(APP_ROOT.'/'.$path.'/default.'.Framework::$views_extension) ) {
+				$view_file = APP_ROOT.'/'.$path.'/default.'.Framework::$views_extension;
+				break;
+			}
+		}
+		
+		if(StatusCode::can_has_body($this->code)){
+  		$this->render_file($view_file);
+		}
     
   }
 
 }
 
-
 class FrameworkError extends Exception {
-  
-  protected $details = null;
-  
+	
+	protected $details = null;
+	
   function __construct($message = null, $details = null, $code = 0) {
-    $this->details = $details;
-    parent::__construct($message, $code);
-  }
-  
-  public function getDetails () {
-    return $this->details;
-  }
+		$this->details = $details;
+		parent::__construct($message, $code);
+	}
+	
+	public function getDetails () {
+		return $this->details;
+	}
 
 }
 
@@ -205,33 +195,18 @@ class FrameworkError extends Exception {
 
 function render_partial(){
   $args = func_get_args();
-  return call_user_func_array(array(Framework::$current_controller_object, 'render_partial'), $args);
+	return call_user_func_array(array(Framework::$current_controller_object, 'render_partial'), $args);
 }
 
 function raise(){
   $args = func_get_args();
-  return call_user_func_array(array(Framework::$current_controller_object, 'raise'), $args);
+	return call_user_func_array(array(Framework::$current_controller_object, 'raise'), $args);
 }
 
-spl_autoload_register('framework_autoload');
-
-function framework_autoload($class_name){
-
-  // check cache if specified class has been found before
-  if ( !empty(Framework::$cache->autoload[$class_name]) ) {
-    if ( is_file(Framework::$cache->autoload[$class_name]) ) {
-      include_once(Framework::$cache->autoload[$class_name]);
-      $found = true;
-    } else {
-      unset(Framework::$cache->autoload[$class_name]);
-      $save_cache = true;
-    }
-  }
-
+function __autoload ($class_name) {
   
   $autoload_paths  = array( APP_ROOT.'/lib/', 
-                            FRAMEWORK_ROOT.'/lib/',
-                            APP_ROOT.'/models/',
+                            APP_ROOT.'/framework/lib/',
                           );
 
   $filename_styles = array( 'lower'=>strtolower($class_name),
@@ -243,25 +218,16 @@ function framework_autoload($class_name){
                             '.class.php',
                           );
 
-  if( empty($found)){
-    foreach($autoload_paths as $path){
-      foreach($filename_styles as $file){
-        foreach($file_extensions as $extension){
-          if( is_file($path.$file.$extension) ){
-            Framework::$prefs->cache->autoload[$class_name] = $path.$file.$extension;
-            $save_cache = true;
-            include_once($path.$file.$extension);
-            break 3;
-          }
+  foreach($autoload_paths as $path){
+    foreach($filename_styles as $file){
+      foreach($file_extensions as $extension){
+        if( is_file($path.$file.$extension) ){
+          include_once($path.$file.$extension);
+          break 3;
         }
       }
     }
   }
-  
-  if ( !empty($save_cache) ) {
-    Framework::$prefs->cache->save();
-  }
-  
   
 }
 
